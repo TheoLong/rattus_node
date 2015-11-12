0d3e0a4c8afa276518f3db877da725006ed73c57
Theo Long <theoloong@gmail.com>
2015-11-12 14:25:56 -0500

    add motor control

---
+++ motor_control/script/moveMotor.py
@@ -0,0 +1,57 @@
+import Adafruit_BBIO.PWM as PWM
+import Adafruit_BBIO.GPIO as GPIO
+
+
+
+
+def moveRight(velocity):
+	print("move right")
+	power=0.0
+	power=velocity*4
+	if power>100.0:
+		power=100.0
+	power=round(power,0)
+	power=int(power)
+	#if power > 0, moving forward
+	if power>0:
+                GPIO.output("P9_11", GPIO.HIGH);
+                GPIO.output("P9_12", GPIO.LOW);
+                PWM.set_duty_cycle("P9_14",power)
+	#if power < 0, move backward
+	if power<0:
+		power=abs(power)
+		GPIO.output("P9_11", GPIO.LOW);
+                GPIO.output("P9_12", GPIO.HIGH);
+                PWM.set_duty_cycle("P9_14",power)
+	#if power = 0, stop
+	if power == 0:
+		GPIO.output("P9_11", GPIO.LOW);
+                GPIO.output("P9_12", GPIO.LOW);
+                PWM.set_duty_cycle("P9_14",0)
+def moveLeft(velocity):
+	print("move left")
+	power=0.0
+	power=velocity*4
+	if power>100.0:
+		power=100.0
+	power=round(power,0)
+	power=int(power)
+	#if power > 0, moving forward
+	if power>0:
+                GPIO.output("P9_13", GPIO.HIGH);
+                GPIO.output("P9_15", GPIO.LOW);
+                PWM.set_duty_cycle("P9_16",power)
+	#if power < 0, move backward
+	if power<0:
+		power=abs(power)
+		GPIO.output("P9_15", GPIO.LOW);
+                GPIO.output("P9_15", GPIO.HIGH);
+                PWM.set_duty_cycle("P9_16",power)
+	#if power = 0, stop
+	if power == 0:
+		power=round(power)
+		GPIO.output("P9_13", GPIO.LOW);
+                GPIO.output("P9_15", GPIO.LOW);
+                PWM.set_duty_cycle("P9_16",0)
+		
+
