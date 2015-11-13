017601c744ab5da313ca205662deefb1b7502155
TheoLong <TheoLoong@gmail.com>
2015-11-12 22:54:41 -0500

    Update encoder_node.py
    pins

--- encoder/script/encoder_node.py
+++ encoder/script/encoder_node.py
@@ -15,18 +15,18 @@ left1="P8_7"
 left2="P8_8"
 right1="P8_9"
 right2="P8_10"
-GPIO.setup(left1, GPIO.IN)
-GPIO.setup(left2, GPIO.IN)
-GPIO.setup(right1, GPIO.IN)
-GPIO.setup(right2, GPIO.IN)
+GPIO.setup("P8_7", GPIO.IN)
+GPIO.setup("P8_8", GPIO.IN)
+GPIO.setup("P8_9", GPIO.IN)
+GPIO.setup("P8_10", GPIO.IN)
 if __name__ == '__main__':
   try:
   	left=JointState()
   	right=JointState()
-  	encoderr.update(GPIO.input(left1),GPIO.input(left2),rospy.get_rostime().to_sec)
+  	encoderr.update(GPIO.input("P8_7"),GPIO.input("P8_8"),rospy.get_rostime().to_sec)
 	left.position.append(encoderr.position) 
 	left.velocity.append(encoderr.velocity)
-	encoderr.update(GPIO.input(right1),GPIO.input(right2),rospy.get_rostime().to_sec)
+	encoderr.update(GPIO.input("P8_9"),GPIO.input("P8_10"),rospy.get_rostime().to_sec)
 	left.position.append(encoderr.position) 
 	left.velocity.append(encoderr.velocity)
 	pubRight = rospy.Publisher('/py_controller/rear_right_wheel/encoder', JointState, queue_size=10)
